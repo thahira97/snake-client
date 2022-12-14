89990355dc1e26aa3c42a586ba9456f813b98cf7
thahira97 <thahirasheerin3@gmail.com>
2022-12-14 16:18:49 -0500

    Add input.js

--- client.js
+++ client.js
@@ -1,30 +1,24 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",
+    host: "192.168.0.100",
     port: 50541,
   });
   conn.setEncoding("utf-8");
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-  });
-  conn.on("connect", () => {
     conn.write("Name: MTS");
   });
   // conn.on("connect", ()=> {
   //   conn.write("Move: up");
-  // })
 
-  conn.on("data", () => {
-    console.log("you ded cuz you idled");
+  // })
+  conn.on("data", (data) => {
+    console.log("Server:", data);
   });
 
   return conn;
 };
-
-console.log("Connecting...");
-connect();
-
-module.exports = "connect";
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,1 +1,6 @@
-const connect = require('./connect');
+const {connect } = require("./client");
+const { setupInput } = require("./input")
+
+console.log("Connecting ...")
+connect();
+setupInput();

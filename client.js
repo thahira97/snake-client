3c031a0ecd39fff5cfd29d3731bced732c713ba9
thahira97 <thahirasheerin3@gmail.com>
2022-12-14 13:35:15 -0500

    Add client.js with some modification

--- client.js
+++ client.js
@@ -1,22 +1,21 @@
 const net = require("net");
 
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541,
   });
   conn.setEncoding("utf-8");
 
-    conn.on("connect", ()=> {
-    console.log("Successfully connected to game server")
-  })
-    conn.on("connect", ()=> {
-      conn.write("Name: MTS")
-    })
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+  conn.on("connect", () => {
+    conn.write("Name: MTS");
+  });
   conn.on("data", () => {
     console.log("you ded cuz you idled");
   });
-
 
   return conn;
 };
@@ -24,4 +23,4 @@ const connect = function() {
 console.log("Connecting...");
 connect();
 
-module.exports = "connect"
+module.exports = "connect";

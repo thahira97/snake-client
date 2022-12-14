3fd08d580870a7fb4eef7f4679f688597ab86632
thahira97 <thahirasheerin3@gmail.com>
2022-12-14 18:38:56 -0500

    Add constants.js

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "192.168.0.100",
-    port: 50541,
+    IP,
+    PORT,
   });
   conn.setEncoding("utf-8");
 
---
+++ constants.js
@@ -0,0 +1,10 @@
+const IP = "192.168.0.100";
+const PORT = 50541;
+
+
+
+
+module.exports = {
+  IP,
+  PORT
+};
